1e162c4ced948f398f8280125ae6f1f755d79510
MrPartis <mrpartisofficial@gmail.com>
2024-11-29 14:13:22 +0700

    modified (again)

--- src/main/java/com/repository/PatientRepository.java
+++ src/main/java/com/repository/PatientRepository.java
@@ -3,6 +3,11 @@ package com.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import com.model.Patient;
 
 @Repository
@@ -14,4 +19,14 @@ public interface PatientRepository extends JpaRepository<Patient,long>{
 
     @Query(value = "SELECT * FROM Patient p WHERE p.DoB = :DoB")
     List<Patient> findDoB(@Param("DoB") Date DoB)
+
+    @Modifying
+    @Transactional
+    @Query(value = "SET FOREIGN_KEY_CHECKS=0", nativeQuery = true)
+    void disableForeignKeyChecks();
+
+    @Modifying
+    @Transactional
+    @Query(value = "SET FOREIGN_KEY_CHECKS=1", nativeQuery = true)
+    void enableForeignKeyChecks();
 }
--- src/main/java/com/service/PatientService.java
+++ src/main/java/com/service/PatientService.java
@@ -2,9 +2,10 @@ package com.service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-import com.model.Patient;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.repository.PatientRepository;
 
 import main.java.com.dto.PatientDTO;
@@ -20,19 +21,82 @@ public class PatientService{
     }
 
     //find by PCode
-    public Patient findPatient(long pcode){
-        Optional<Patient> patient = this.patient_repo.findPCode(pcode);
-        if (patient.isPresent()){
-            return patient.get();
-        } else {
-            return null;
+    @Transactional
+    public Patient findPatient(long code){
+        Optional<Patient> targetPatient = this.patient_repo.findPCode(code);
+
+        if (targetPatient.isPresent()){
+            return targetPatient.get();
         }
+        return null;
     }
 
+    @Transactional
     public List<Patient> getAllPatient(Patient patient){
         List<Patient> patients = patient_repo.findAll();
 
-        return patients.stream().map(patient -> new PatientDTO(patient)).collect(Collectors.toList());
+        return patients.stream().map(PatientDTO::new).toList();
     }
 
+    public PatientDTO getOnePatient(long code){
+        Patient patient = this.findPatient(code);
+
+        if (patient == null){
+            throw new RuntumeException("Patient not found");
+        }
+        return new PatientDTO(patient);
+    }
+
+    //create new patient
+    @Transactional
+    public Patient createPatient(PatientDTO patientDTO){
+        System.out.println(patientDTO);
+
+        Patient newPatient = new Patient();
+
+        this.patient_repo.disableForeignKeyChecks();
+
+        newPatient.setFirstname(patientDTO.getFirstname());
+        newPatient.setLastname(patientDTO.getLastname());
+        newPatient.setdob(patientDTO.getdob());
+        newPatient.setGender(patientDTO.getGender());
+        newPatient.setAdress(patientDTO.getAdress());
+        newPatient.setPatienttype(patientDTO.getPatienttype());
+
+        this.patient_repo.enableForeignKeyChecks();
+        return patient_repo.save(newPatient);
+    }
+
+    //update patient info
+    @Transactional
+    public PatientDTO updatePatient(long code, PatientDTO patientDTO){
+        Patient updated = this.findPatient(code);
+
+        if (updated==null){
+            throw new RuntimeException("Patient not found");
+        }
+
+        updated.setFirstname(patientDTO.getFirstname());
+        updated.setLastname(patientDTO.getLastname());
+        updated.setdob(patientDTO.getdob());
+        updated.setGender(patientDTO.getGender());
+        updated.setAdress(patientDTO.getAdress());
+        updated.setPatienttype(patientDTO.getPatienttype());
+        Patient saved = patient_repo.save(updated);
+        return new PatientDTO(saved);
+    }
+
+    //clear patient list
+    @Transactional
+    public void handleDeleteAllEmployees() {
+        patient_repo.deleteAll();
+    }
+    
+    //delete patient
+    @Transactional
+    public void deletePatient(long code){
+        Patient patient = this.findPatient(code);
+        if (patient == null) {throw new RuntimeException("Patient not found");}
+        patient_repo.delete(patient);
+    }
 }
