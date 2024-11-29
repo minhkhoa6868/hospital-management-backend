e3efa3afb7fb9f7d02e2a71be900f28edb446737
MrPartis <76799029+MrPartis@users.noreply.github.com>
2024-11-29 20:31:32 +0700

    Revert "revert back"

--- src/main/java/com/Medication.java
+++ src/main/java/com/Medication.java
@@ -1,6 +1,7 @@
 import java.sql.Date;
 
-import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -10,7 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-import main.java.com.model.MedStatus;
+import jakarta.presistence.*;
 
 @Entity
 @Table(name="Medication")
@@ -31,10 +32,14 @@ public class Medication{
     private Integer Quantity;
 
     @Temporal(TemporalType.DATE)
-    @Column(nullable = false, name = "expiration_date")
+    @Column(nullable = false, name = "Expiration Date")
     private Date ExpirationDate;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "m_status", nullable = false)
-    private MedStatus Status;
+    @Column(name = "m_status", nullable = false, columnDefinition = "ENUM('AVAILABLE', 'OUT_OF_STOCK', 'EXPIRED')")
+    private MedicaionStatus Status;
+
+    public MedicationStatus getStatus() {
+        return Status;
+    }
 }
